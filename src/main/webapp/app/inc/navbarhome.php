fe82ec62f900f999a5adfb7d9fa4c3e5eaaf2109
Dave Messer <>
2015-10-12 14:54:48 -0500

    Adds Web UI

---
+++ src/main/webapp/app/inc/navbarhome.php
@@ -0,0 +1,13 @@
+<!-- Nav Bar Home -->
+<header class="mdl-layout__header mdl-layout__header--fixed">
+<div class="mdl-layout__header-row">
+  <!-- Title -->
+  <span class="mdl-layout-title">Mood Mapper</span>
+  <!-- Add spacer, to align navigation to the right -->
+  <div class="mdl-layout-spacer"></div>
+  <!-- Navigation -->
+  <nav class="mdl-navigation">
+    <a class="mdl-navigation__link" href="">About</a>
+  </nav>
+</div>
+</header>
---
+++ src/main/webapp/app/inc/navdrawer.php
@@ -0,0 +1,11 @@
+<!-- Nav Drawer -->
+<div class="mdl-layout__drawer">
+	<span class="mdl-layout-title">Mood Mapper</span>
+	<nav class="mdl-navigation">
+		<a class="mdl-navigation__link" href="/app/"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i>Home</a>
+		<a class="mdl-navigation__link" href="/app/my-groups/"><i class="material-icons">people</i>My Groups</a>
+		<a class="mdl-navigation__link" href="/app/mood-maps/"><i class="material-icons">insert_chart</i>Mood Maps</a>
+		<a class="mdl-navigation__link" href="/app/new-status"><i class="material-icons">add_circle</i>New Status</a>
+		<a class="mdl-navigation__link" href=""><i class="material-icons">exit_to_app</i>Sign out</a>
+	</nav>
+</div>
